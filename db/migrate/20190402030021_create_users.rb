d101ba4e305b2840ebb26214bcde09f3f607f289
vantri0964 <vantri27051998@gmail.com>
2019-04-13 10:34:58 +0700

    heroku up

---
+++ db/migrate/20190402030021_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :userName
+      t.string :passWord
+      t.string :phoneNumber
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
